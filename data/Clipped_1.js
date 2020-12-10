3903f0e5438916b1c253491af30a8e6381d273d7
jakobson-broid <75787826+jakobson-broid@users.noreply.github.com>
2020-12-10 16:55:29 +0300

    Add files via upload

---
+++ data/Clipped_1.js
@@ -0,0 +1,13 @@
+var json_Clipped_1 = {
+"type": "FeatureCollection",
+"name": "Clipped_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "4", "photo": "Kebena", "Type": "Sefer" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.777653048256447, 9.033502314383984 ] ] } },
+{ "type": "Feature", "properties": { "id": "6", "photo": "Giyorgis", "Type": "Square" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.753576168837846, 9.035856573148603 ] ] } },
+{ "type": "Feature", "properties": { "id": "8", "photo": "Semen Mazegaja", "Type": "Sefer" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.739968785866452, 9.052755576339383 ] ] } },
+{ "type": "Feature", "properties": { "id": "9", "photo": "Gedan sefer", "Type": "Sefer" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.74437987062857, 9.040582285383273 ] ] } },
+{ "type": "Feature", "properties": { "id": "10", "photo": "Amst Killo", "Type": "Square" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.761451883551494, 9.04031666601187 ] ] } },
+{ "type": "Feature", "properties": { "id": "12", "photo": "Arat Killo", "Type": "Square" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.767447673648078, 9.032228273238404 ] ] } }
+]
+}
